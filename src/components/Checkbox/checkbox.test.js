d6de55faf8de73c6bf41be1f2046395f90e20ccb
Kuliza-280 <psnitsri01@gmail.com>
2020-08-20 20:31:49 +0530

    Adding Checkbox component

---
+++ src/components/Checkbox/checkbox.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Checkbox from "./index";
+
+test("render Checkbox component", () => {
+  render(<Checkbox />);
+});
---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,24 @@
+import React, { useCallback } from "react";
+import PropTypes from "prop-types";
+
+function Checkbox({ value, onChange, ...props }) {
+  const handleOnChange = useCallback((e) => onChange(e, value), [
+    onChange,
+    value,
+  ]);
+  return (
+    <input type="checkbox" value={value} onChange={handleOnChange} {...props} />
+  );
+}
+
+Checkbox.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+Checkbox.defaultProps = {
+  value: "",
+  onChange: () => {},
+};
+
+export default Checkbox;
