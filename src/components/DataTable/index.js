73652a72e346a819659a1c85157b7f3250b29a2b
Kuliza-280 <psnitsri01@gmail.com>
2020-08-20 18:34:30 +0530

    Rendering row data and adding rowClick event

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -7,20 +7,29 @@ function DataTable({ columns, rows, onRowClick, onSelectionChange }) {
   const renderColumns = (col) => (
     <th
       key={col.id}
-      style={{ width: col.width || `${100 / (columns.length + 1)}%` }}
+      style={{ width: col.width || `${90 / (columns.length + 1)}%` }}
     >
       {col.label}
     </th>
   );
 
-  const renderRows = (row) => {};
+  const renderRows = (row, i) => (
+    <tr className="data-row" onClick={() => onRowClick(row, i)} key={row.id}>
+      {columns.map((col) => (
+        <td className={col.numeric ? "numeric-col" : ""} key={col.id}>
+          {row[col.id]}
+        </td>
+      ))}
+      <td className="center">Select</td>
+    </tr>
+  );
 
   return (
     <table>
       <tbody>
         <tr>
           {columns.length > 0 && columns.map(renderColumns)}
-          <th>Select</th>
+          <th className="select-col">Select</th>
         </tr>
         {rows.length > 0 ? (
           rows.map(renderRows)
--- src/screens/App/App.js
+++ src/screens/App/App.js
@@ -18,9 +18,30 @@ function App() {
     },
   ];
 
+  const data = [
+    {
+      id: 1,
+      name: "Ajay",
+      age: 20,
+    },
+    {
+      id: 2,
+      name: "Anil",
+      age: "26",
+    },
+  ];
+
+  const handleRowClick = (...args) => {
+    console.log(...args);
+  };
+
   return (
     <div className="wrapper">
-      <DataTable columns={columnsConfig} />
+      <DataTable
+        columns={columnsConfig}
+        rows={data}
+        onRowClick={handleRowClick}
+      />
     </div>
   );
 }
