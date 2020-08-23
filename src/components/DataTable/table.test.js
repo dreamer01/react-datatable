45ed2ad6a5c0c0a446dde751bf1f82c9b5e3089c
Kuliza-280 <psnitsri01@gmail.com>
2020-08-23 19:57:10 +0530

    Adding test for functions and adding Github CI Test run

--- src/components/DataTable/table.test.js
+++ src/components/DataTable/table.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import DataTable from "./index";
 
@@ -22,6 +23,9 @@ const rowData = [
   { id: 2, name: "B", age: 10 },
 ];
 
+const onRowClick = jest.fn();
+const onSelectionChange = jest.fn();
+
 test("render DataTable component", () => {
   const { getAllByRole, rerender } = render(
     <DataTable columns={columnsConfig} />
@@ -38,4 +42,35 @@ test("render DataTable component", () => {
   expect(getAllByRole("row").length).toBe(
     rowsBeforeData.length + rowData.length - 1 // Minus 1 for Not Found Row
   );
+});
+
+test("render DataTable and prop function onRowClick is invoked", () => {
+  const { getAllByRole } = render(
+    <DataTable
+      columns={columnsConfig}
+      rows={rowData}
+      onRowClick={onRowClick}
+      onSelectionChange={onSelectionChange}
+    />
+  );
+
+  userEvent.click(getAllByRole("row")[2]); // First row with data
+  expect(onRowClick).toHaveBeenCalledTimes(1);
+  expect(onRowClick).toHaveBeenCalledWith(rowData[0], 0);
+});
+
+test("render DataTable and prop function onSelectionChange is invoked", () => {
+  const { getAllByRole } = render(
+    <DataTable
+      columns={columnsConfig}
+      rows={rowData}
+      onSelectionChange={onSelectionChange}
+    />
+  );
+
+  userEvent.click(getAllByRole("checkbox")[0]); // Header Checkbox
+  expect(onSelectionChange).toHaveBeenCalledWith("All");
+
+  userEvent.click(getAllByRole("checkbox")[1]); // First row with data
+  expect(onSelectionChange).toHaveBeenCalledWith(["1"]);
 });
