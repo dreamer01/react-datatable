5454010e96780c3d5b767df1a2d1e31649aef4e8
Kuliza-280 <psnitsri01@gmail.com>
2020-08-26 13:57:17 +0530

    Adding Test for search

--- src/screens/App/App.test.js
+++ src/screens/App/App.test.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { render, waitForElement } from "@testing-library/react";
+import { render, waitForElement, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("on render App page make request", async () => {
   const { getByText } = render(<App />);
   await waitForElement(() =>
     getByText(/accusamus beatae ad facilis cum similique qui sunt/i)
+  );
+});
+
+test("render data search string", async () => {
+  const { getByText, getByRole } = render(<App />);
+  fireEvent.change(getByRole("searchbox"), { target: { value: "rep" } });
+  await waitForElement(() =>
+    getByText(/reprehenderit est deserunt velit ipsam/i)
   );
 });
